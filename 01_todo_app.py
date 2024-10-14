5cbebcd965e1c8bdf742face607728d14d6ec084
Vanshika Saxena <saxenavanshika22@gmail.com>
2024-10-14 17:02:31 +0530

    at day 4

--- 01_todo_app.py
+++ 01_todo_app.py
@@ -1,8 +1,21 @@
-user_prompt="Enter a todo:"
-todo1=input(user_prompt)
-todo2=input(user_prompt)
-todo3=input(user_prompt)
-todos=[todo1,todo2,todo3]
-print(todos)
-# print(type(user_prompt))
-# print(type(todos))
+
+todos=[]
+while True:
+    user_action=input("Type add ,edit, show or exit : ")
+    user_action=user_action.strip()
+    if user_action=='add':
+        todo=input("Enter a todo:")
+        todos.append(todo.capitalize())
+    elif user_action=='show'or user_action=='display':
+        for item in todos:
+            print(item.title())
+    elif user_action=='exit':
+        break
+    elif user_action=='edit':
+        number=int(input("Number of the todo to edit : "))
+        new_todo=input("Enter edited todo : ")
+        todos[number-1]=new_todo
+        print("Editing completed")
+    else:
+        print("You enter the wrong command, type add,edit,show or exit")
+
--- 02_len().py
+++
@@ -1,8 +0,0 @@
-#goal is to understand the len() ,returns a number
-
-text=input("enter a title")
-
-length=len(text)
-
-# print(length)
-print("The length of the title is ",length)
